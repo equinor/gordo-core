cc05d739dd42af323e5ab77c66c20be920ed0d15
Serhii Koropets <skoropets@gmail.com>
2022-08-20 11:31:05 +0300

    scripts/check_version.sh

---
+++ scripts/check_version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+function usage {
+    echo "Usage: $0 [VERSION]"
+    echo "Compare provided version with the version in pyproject.toml file. Exit with code 1 if versions differ"
+    echo ""
+    echo "  -h  display this help and exit"
+    exit $1
+}
+
+if [ "$1" = "-h" ]; then
+    usage 0
+fi
+
+version=$1
+if [ -z "$version" ]; then
+    usage 1
+fi
+
+function check_package_version {
+    grep "^version = \"*\"" pyproject.toml | grep $1
+}
+
+package_version=${version#"v"}
+if check_package_version "$package_version"; then
+    echo "$version is a valid version"
+else
+    echo "$version is not a valid version" 1>&2
+    exit 1
+fi
