0f2b8cf971b2fab2185f736b36b0a5d23f7dee00
Nicolas Petitclerc <npetitclerc@gmail.com>
2022-12-07 13:08:00 +0100

    Fixed test all

--- tests/filters/test_periods.py
+++ tests/filters/test_periods.py
@@ -27,14 +27,19 @@ def test_filter_periods_typerror(data):
 
 def test_filter_periods_quantile(data):
     data_filtered, drop_periods, predictions = FilterPeriods(
-        granularity="10T", filter_method="quantile", n_iqr=1
+        granularity="10T",
+        filter_method="quantile",
+        n_iqr=1,
+        quantile_upper=0.8,
+        quantile_lower=0.2,
     ).filter_data(data)
     print("Quantile test results:")
-    print(sum(predictions["quantile"]["pred"])
+    print(sum(predictions["quantile"]["pred"]))
     print(len(drop_periods["quantile"]))
-    assert sum(predictions["quantile"]["pred"]) == -377
-    assert len(drop_periods["quantile"]) == 26
-    assert data_filtered.shape == (4513, 1)
+    print(data_filtered.shape)
+    assert sum(predictions["quantile"]["pred"]) == -96
+    assert len(drop_periods["quantile"]) == 4
+    assert data_filtered.shape == (4794, 1)
 
 def test_filter_periods_median(data):
     data_filtered, drop_periods, predictions = FilterPeriods(
@@ -58,16 +63,21 @@ def test_filter_periods_iforest(data):
 
 def test_filter_periods_all(data):
     data_filtered, drop_periods, predictions = FilterPeriods(
-        granularity="10T", filter_method="all", n_iqr=1, iforest_smooth=False
+        granularity="10T",
+        filter_method="all",
+        n_iqr=1,
+        iforest_smooth=False,
+        quantile_upper=0.8,
+        quantile_lower=0.2,
     ).filter_data(data)
 
-    assert sum(predictions["quantile"]["pred"]) == -377
-    assert len(drop_periods["quantile"]) == 26
+    assert sum(predictions["quantile"]["pred"]) == -96
+    assert len(drop_periods["quantile"]) == 4
     assert sum(predictions["median"]["pred"]) == -377
     assert sum(predictions["iforest"]["pred"]) == 4596
     assert len(drop_periods["median"]) == 26
     assert len(drop_periods["iforest"]) == 16
-    assert data_filtered.shape == (4386, 1)
+    assert data_filtered.shape == (4352, 1)
 
 
 def test_filter_periods_iforest_smoothing(data):
